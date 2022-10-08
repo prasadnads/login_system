d22b9d835803873f07282bb58c523bf30ad30817
prasadnads <prasadnads@gmail.com>
2022-10-08 06:47:21 +0530

    commit changes

--- login/settings.py
+++ login/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-ln8)q9#bxvkh*nbe7v9n*cj1i8w!qq!5&*u4dgl3b!fr(ncg8c"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
